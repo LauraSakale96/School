93855f053def6562bbbea7c94268f461fca76552
Laura Sakale <laura.sakale96@gmail.com>
2018-05-13 20:49:14 +0300

    Profile temporary view (need to add image place), profile table etc(changeed the minor error)

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('profiles', 'ProfilesController');
+Route::get('profiles', 'ProfilesController@index');
 
